9bc1b3bb5ac020d617c914e3bbdcd799710c2752
AgusBarba03 <agustinbarbalase@gmail.com>
2022-03-07 11:09:28 -0300

    Adding routes (users and sessions) and middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api", require("./routes/sessions.routes"));
+app.use("/api/users", require("./routes/users.routes"));
+
+app.use(require("./middlewares/handleErrors"));
+
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
 });
