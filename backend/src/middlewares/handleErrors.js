cace8e4684cedadf7de915d2123c6378f4f3bb21
AgusBarba03 <agustinbarbalase@gmail.com>
2022-03-10 11:24:51 -0300

    Adding errors to handleErrors

--- backend/src/middlewares/handleErrors.js
+++ backend/src/middlewares/handleErrors.js
@@ -1,11 +1,36 @@
 const LIST_OF_ERRORS = {
+  P2002: (res, error) => {
+    res.status(400).send({
+      message: `${error.meta.target[0].capitalizate()} has been register`,
+    });
+  },
+  JsonWebTokenError: (res, error) => {
+    res.status(400).send({
+      message: "Token missing or invalid",
+    });
+  },
+  TokenExpiredError: (res, error) => {
+    res.status(400).send({
+      message: "Token missing or invalid",
+    });
+  },
+  Unauthenticated: (res, error) => {
+    res.status(400).send({
+      message: "Token missing or invalid",
+    });
+  },
+  Unauthorizated: (res, error) => {
+    res.status(401).send({
+      message: "Unauthorizated",
+    });
+  },
   defaultError: (res, error) => {
-    console.error(error);
+    console.log(error.name, error.code);
     res.status(500).end();
-  }
+  },
 };
 
 module.exports = (error, req, res, next) => {
-  const handler = LIST_OF_ERRORS[error.name] || LIST_OF_ERRORS.defaultError;
+  const handler = LIST_OF_ERRORS[error.message] || LIST_OF_ERRORS.defaultError;
   handler(res, error);
-}
+};
