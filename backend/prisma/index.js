02eb714a78f0214b107adcf9d594b76798f39e9d
AgusBarba03 <agustinbarbalase@gmail.com>
2022-03-10 11:20:02 -0300

    Adding controllers to Prisma

--- backend/prisma/index.js
+++ backend/prisma/index.js
@@ -1,5 +1,11 @@
 const userPrisma = require("./user.prisma");
+const channelPrisma = require("./channel.prisma");
+const chatPrisma = require("./chat.prisma")
+const messagePrisma = require("./message.prisma");
 
 module.exports = {
   userPrisma,
+  channelPrisma,
+  chatPrisma,
+  messagePrisma
 };
