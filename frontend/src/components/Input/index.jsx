71e57e1a02eb48b824b8a5f1d43f5c45e2945b49
AgusBarba03 <agustinbarbalase@gmail.com>
2022-03-13 19:33:17 -0300

    Changing Input from object mode to component mode and Adding hover and focus

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
-const Input = styled.input((props) => ({
-  padding: ".5rem",
-  fontSize: props.type === "submit" ? "1.5rem" : "1.3rem",
-  border: props.type === "submit" ? "none" : "1px solid #888",
-  color: props.type === "submit" ? "#fff" : "#000",
-  backgroundColor: props.type === "submit" ? "#4A154B" : "#fff",
-  fontWeight: props.type === "submit" ? "500" : "300",
-}));
+const Input = styled.input`
+  padding: 0.5rem;
+  font-size: ${(props) => (props.type === "submit" ? "1.5rem" : "1.3rem")};
+  border: ${(props) => (props.type === "submit" ? "none" : "1px solid #888")};
+  color: ${(props) => (props.type === "submit" ? "#fff" : "#000")};
+  background-color: ${(props) =>
+    props.type === "submit" ? "#4A154B" : "#fff"};
+  font-weight: ${(props) => (props.type === "submit" ? "500" : "300")};
+  &:hover,
+  &:focus {
+    background-color: ${(props) =>
+      props.type === "submit" ? "rgba(74, 21, 75, .8)" : "#fff"};
+    cursor: pointer;
+  }
+`;
 
 export default Input;
